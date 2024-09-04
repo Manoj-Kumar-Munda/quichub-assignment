ba2e642ce6d6eaae0e1d9e0936617df602ef5346
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-09-04 13:25:26 +0530

    build errors fixed

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -1,8 +1,7 @@
-// Import necessary modules
+
 import fs from 'fs/promises';
 import Papa from 'papaparse';
 
-// Function to read the CSV file and convert it to an array of phone numbers
 export async function getPhoneNumbersFromCSV(filePath) {
     try {
         const data = await fs.readFile(filePath, 'utf8');
@@ -12,7 +11,7 @@ export async function getPhoneNumbersFromCSV(filePath) {
         });
         console.log(results);
 
-        // Extract phone numbers into an array
+  
         const phoneNumbers = results.data.map(row => row['Mobile Number']);
         return phoneNumbers;
     } catch (err) {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { getPhoneNumbersFromCSV } from "../../../backend/src/utils/helpers";
 
 const Home = () => {
   const [campaignName, setCampaignName] = useState("");
