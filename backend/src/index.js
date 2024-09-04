23e05713361abd1235ba7a64d652b6f7eb347582
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-09-04 12:48:42 +0530

    added dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,11 @@
 import app from "./app.js";
+import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
+
+
+dotenv.config({
+  path: './env'
+});
 
 const PORT = process.env.PORT || 8000;
 
